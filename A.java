ad69ea64d0a094453813890cbe58a69ecdb3fe84
1986sabyasachee <50901438+1986sabyasachee@users.noreply.github.com>
2025-09-09 13:55:40 +0530

    Update 2 A.java

--- A.java
+++ A.java
@@ -1,16 +1,20 @@
+import java.util.*;
 class A
 {
   int a,b;
   void setter(int a1,int b1)
   {
+    
+    
     a=a1;
     b=b1;
   System.out.println("settre method");
   }
   void getter()
   {
-    
-    System.out.println("getter ...."+ (a+b));
+    Scanner sc=new Scanner(System.in);
+    int c=sc.nextInt();
+    System.out.println("getter ...."+ (a+b+c));
   }
 }
 //  public static void main(String args[])
